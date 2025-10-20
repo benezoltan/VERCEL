2103ab95ae34a0f0f10a6efc53689dcb9b412833
benezoltan <bene.zoltan@telekimezotur.hu>
2025-10-20 09:42:30 +0200

    step4

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
+// Egyszerű route
 app.get("/", (req, res) => {
   res.send("Hello from Express on Vercel!");
 });
 
+// Vercel port beállítása
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
